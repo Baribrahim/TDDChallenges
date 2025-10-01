db5c02cc48acecf1e29b0493badc6c28de8e42ec
Abdul Bari <barii02@hotmail.com>
2025-10-01 11:58:00 +0100

    EDIT: added some comments to the test file

--- test_calculator.py
+++ test_calculator.py
@@ -3,17 +3,23 @@ import pytest
 from Calculator import Calculator
 
 def test_add_one_plus_one_gives_two():
+    # Arrange
     calc = Calculator()
     num1 = 1
     num2 = 1
     expected_answer = 2
+    # Act
     result = calc.add(num1, num2)
+    # Assert
     assert expected_answer == result
 
 def test_add_one_plus_two_gives_three():
+    # Arrange
     calc = Calculator()
     num1 = 1
     num2 = 2
     expected_answer = 3
+    # Act
     result = calc.add(num1, num2)
+    # Assert
     assert expected_answer == result
