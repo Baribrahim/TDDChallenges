e7a0bc12631eb6000d8e5bb79935a3455b332f4e
Ano <81051693+Mushroom-Ano@users.noreply.github.com>
2025-10-01 12:05:38 +0100

    UPDATE: Created a new sub method, with a return of 0 for testing alongside a test function for subtract.

--- test_calculator.py
+++ test_calculator.py
@@ -23,3 +23,14 @@ def test_add_one_plus_two_gives_three():
     result = calc.add(num1, num2)
     # Assert
     assert expected_answer == result
+
+def test_sub_two_minus_one_gives_one():
+    # Arrange
+    calc = Calculator()
+    num1 = 2
+    num2 = 1
+    expected_answer = 1
+    # Act
+    result = calc.sub(num1, num2)
+    # Assert
+    assert expected_answer == result
