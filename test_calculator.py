1f557b3b91e4167b97257aee3801bc9f1dee02f4
Ano <81051693+Mushroom-Ano@users.noreply.github.com>
2025-10-01 12:28:41 +0100

    UPDATE: added test_add with parameterised values and fixture

--- test_calculator.py
+++ test_calculator.py
@@ -6,42 +6,54 @@ from Calculator import Calculator
 def calc():
     return Calculator()
 
-def test_add_one_plus_one_gives_two(calc):
-    # Arrange
-    num1 = 1
-    num2 = 1
-    expected_answer = 2
-    # Act
-    result = calc.add(num1, num2)
-    # Assert
-    assert expected_answer == result
+@pytest.mark.parameterize(
+    "A, B, Expected",
+    [
+        (1, 4, 5)
+        (-1, 1, 0)
+    ]
+)
 
-def test_add_one_plus_two_gives_three(calc):
-    # Arrange
-    num1 = 1
-    num2 = 2
-    expected_answer = 3
-    # Act
-    result = calc.add(num1, num2)
-    # Assert
-    assert expected_answer == result
+def test_add(calc, a, b, expected):
+    assert calc.add(a, b) == expected
+    
 
-def test_sub_two_minus_one_gives_one(calc):
-    # Arrange
-    num1 = 2
-    num2 = 1
-    expected_answer = 1
-    # Act
-    result = calc.sub(num1, num2)
-    # Assert
-    assert expected_answer == result
+# def test_add_one_plus_one_gives_two(calc):
+#     # Arrange
+#     num1 = 1
+#     num2 = 1
+#     expected_answer = 2
+#     # Act
+#     result = calc.add(num1, num2)
+#     # Assert
+#     assert expected_answer == result
 
-def test_sub_four_minus_one_gives_three(calc):
-    # Arrange
-    num1 = 4
-    num2 = 1
-    expected_answer = 3
-    # Act
-    result = calc.sub(num1, num2)
-    # Assert
-    assert expected_answer == result
+# def test_add_one_plus_two_gives_three(calc):
+#     # Arrange
+#     num1 = 1
+#     num2 = 2
+#     expected_answer = 3
+#     # Act
+#     result = calc.add(num1, num2)
+#     # Assert
+#     assert expected_answer == result
+
+# def test_sub_two_minus_one_gives_one(calc):
+#     # Arrange
+#     num1 = 2
+#     num2 = 1
+#     expected_answer = 1
+#     # Act
+#     result = calc.sub(num1, num2)
+#     # Assert
+#     assert expected_answer == result
+
+# def test_sub_four_minus_one_gives_three(calc):
+#     # Arrange
+#     num1 = 4
+#     num2 = 1
+#     expected_answer = 3
+#     # Act
+#     result = calc.sub(num1, num2)
+#     # Assert
+#     assert expected_answer == result
